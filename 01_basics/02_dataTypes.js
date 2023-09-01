973f32967838ba34c6f02f3efee0bf200c68fb36
manvideore21 <137396100+manvideore21@users.noreply.github.com>
2023-09-01 07:44:23 +0000

    dataTypes and variables

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all Javascript code as newer version
+
+//alert('MANVI') // WE ARE USING NODE.JS, NOT BROWSER
+
+console.log(3+3)  //code readablility should be high
+
+let name = "Manvi"
+let age = 21
+let isLoggedIn = false
+let num = null 
+
+console.log([name, age, isLoggedIn, num]);
+console.log(typeof null);
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "manvideore232@gmail.com"
+var accountPassword = "123456"
+accountCity ="Pune"
+let accountState;
+
+
+//accountId = 2
+accountEmail = "manvi"
+accountPassword = "11111"
+accountCity= "Nashik"
+
+console.log(accountId);
+
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
